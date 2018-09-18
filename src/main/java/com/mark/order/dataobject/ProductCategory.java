d9c1f19ed706bc5e3d26d8d3bafdb56f302b6022
Li Han <hiliguoxin@foxmail.com>
2018-09-18 17:48:30 +0800

    DAO-ProducrtCategory
    实体类、Repository、测试类

--- src/main/java/com/mark/order/dataobject/ProductCategory.java
+++ src/main/java/com/mark/order/dataobject/ProductCategory.java
@@ -1,5 +1,8 @@
 package com.mark.order.dataobject;
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,6 +12,8 @@ import java.util.Date;
  * 类目
  */
 @Entity
+@DynamicUpdate //只更新修改的字段信息
+@Data //lombok工具提供，注解在类上, 为类提供get/set方法, 此外还提供了 equals()、hashCode()、toString() 方法
 public class ProductCategory {
 
     /** 类目ID */
@@ -27,62 +32,11 @@ public class ProductCategory {
     private Date updateTime;
 
 
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
     public ProductCategory() {
     }
 
     public ProductCategory(String categoryName, Integer categoryType) {
         this.categoryName = categoryName;
         this.categoryType = categoryType;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
     }
 }
--- src/test/java/com/mark/order/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/mark/order/repository/ProductCategoryRepositoryTest.java
@@ -6,6 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +25,32 @@ public class ProductCategoryRepositoryTest {
     public void findOne(){
       ProductCategory productCategory =  repository.findOne(1);
       System.out.println(productCategory);
+    }
+
+    @Test
+    public void findAll(){
+        List<ProductCategory> productCategories =  repository.findAll();
+        Assert.assertNotNull(productCategories);
+        System.out.println(productCategories);
+    }
+
+    @Test
+    @Transactional //测试数据会完全回滚
+    public void saveOne(){
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("低价帮");
+        productCategory.setCategoryType(4);
+        ProductCategory result = repository.save(productCategory);
+        Assert.assertNotEquals(null,result);
+    }
+
+    @Test
+    public void findByCategoryTypeInTest(){
+        List<Integer> categoryType = Arrays.asList(2,3,4);
+
+        List<ProductCategory> result = repository.findByCategoryTypeIn(categoryType);
+
+        Assert.assertNotEquals(0,result.size());
     }
 
 }
