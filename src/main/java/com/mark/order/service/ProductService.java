6074f1dad5f411afa766b0fe506efd84e01e5bea
Li Han <hiliguoxin@foxmail.com>
2018-12-07 16:53:12 +0800

    完成商品查询service层
    真的是懒

---
+++ src/main/java/com/mark/order/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.mark.order.service;
+
+import com.mark.order.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * 商品
+ */
+public interface ProductService {
+
+    ProductInfo findOne(String productId);
+
+    /**
+     * 查询所有在架商品列表
+     * @return
+     */
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+
+    //加库存
+
+    //减库存
+
+}
